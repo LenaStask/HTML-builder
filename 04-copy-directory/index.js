5395769076569c20ba4a9333fbe9fe289fb5bc33
lenaStsk <staskevichalena@gmail.com>
2022-05-24 21:57:03 +0300

    fix: remove directory before copying files

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -2,13 +2,13 @@ const fs = require('fs');
 const path = require('path');
 const dir = path.join(__dirname, 'files');
 const copyDir = path.join(__dirname, 'files-copy');
-fs.promises.mkdir(copyDir, {recursive: true})
-  .then (() => copy())
-  .catch ((err) => {
-    if(err) {
-      console.log(err);
-    }
-  });
+fs.promises.rm(copyDir, {recursive: true, force: true})
+  .then(() => {
+    fs.promises.mkdir(copyDir, {recursive: true})
+      .then (() => copy())
+      .catch ((err) => console.log(err));
+  })
+  .catch ((err) => console.log(err));
 function copy() {
   fs.readdir(dir, (err, files) => {
     if (err) {
